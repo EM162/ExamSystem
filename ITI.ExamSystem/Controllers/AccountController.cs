26ca52799ca15152a6a749e59376cf7e2dc89447
dareen3laa <dareen3laa2002@gmail.com>
2025-04-16 00:39:52 +0200

    admin login and dashboard

---
+++ ITI.ExamSystem/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.ExamSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ ITI.ExamSystem/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.ExamSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
